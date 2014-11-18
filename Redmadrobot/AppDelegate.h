365c51ffc37ac32d620eda5e83b70d289e716941
Dmitry Shashlov <shashlov@gmail.com>
2014-11-18 13:46:29 +0300

    Initial Commit

---
+++ Redmadrobot/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Redmadrobot
+//
+//  Created by Dmitry Shashlov on 11/18/14.
+//  Copyright (c) 2014 Dmitry Shashlov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Redmadrobot/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Redmadrobot
+//
+//  Created by Dmitry Shashlov on 11/18/14.
+//  Copyright (c) 2014 Dmitry Shashlov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
