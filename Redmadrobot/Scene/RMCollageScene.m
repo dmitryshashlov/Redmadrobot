01c00ef34feeb256eedb47af4abe545be29168a9
Dmitry Shashlov <shashlov@gmail.com>
2014-11-19 14:17:03 +0300

    grid scene implementation

--- Redmadrobot/Scene/RMCollageScene.m
+++ Redmadrobot/Scene/RMCollageScene.m
@@ -14,6 +14,10 @@ static CGSize kCollageSize = { 288.0f , 288.0f };
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+@interface RMCollageScene()
+@property (nonatomic) SKShapeNode *perimeterNode;
+@end
+
 @implementation RMCollageScene
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -29,11 +33,48 @@ static CGSize kCollageSize = { 288.0f , 288.0f };
     _collage = collage;
     self.backgroundColor = [UIColor whiteColor];
     
-    SKShapeNode *baseNode = [SKShapeNode shapeNodeWithRect:CGRectMake(0.0f, 0.0f, kCollageSize.width, kCollageSize.height)];
-    baseNode.position = CGPointMake((size.width - kCollageSize.width) / 2, (size.height - kCollageSize.height) / 2);
-    baseNode.strokeColor = [UIColor blackColor];
-    baseNode.lineWidth = 1.0f;
-    [self addChild:baseNode];
+    // Perimeter node
+    _perimeterNode = [SKShapeNode shapeNodeWithRect:CGRectMake(0.0f, 0.0f, kCollageSize.width, kCollageSize.height)];
+    _perimeterNode.position = CGPointMake((size.width - kCollageSize.width) / 2, (size.height - kCollageSize.height) / 2);
+    [self addChild:_perimeterNode];
+    
+    // Line nodes
+    NSMutableArray *lines = [[NSMutableArray alloc] initWithObjects:_perimeterNode, nil];
+    for (int i = 1; i < collage.size.intValue; i++) {
+      CGFloat gridStep = i * (kCollageSize.width / collage.size.intValue);
+      CGFloat linePadding = (size.height - kCollageSize.height) / 4;
+
+      // Vertical points
+      CGPoint verticalPoints[2];
+      verticalPoints[0] = CGPointMake(gridStep, -linePadding);
+      verticalPoints[1] = CGPointMake(gridStep, kCollageSize.height + linePadding);
+      
+      // Horizontal points
+      CGPoint horizontalPoints[2];
+      horizontalPoints[0] = CGPointMake(-linePadding, gridStep);
+      horizontalPoints[1] = CGPointMake(kCollageSize.width + linePadding, gridStep);
+
+      SKShapeNode *verticalLine = [SKShapeNode shapeNodeWithPoints:verticalPoints count:sizeof(verticalPoints) / sizeof(CGPoint)];
+      SKShapeNode *horizontalLine = [SKShapeNode shapeNodeWithPoints:horizontalPoints count:sizeof(horizontalPoints) / sizeof(CGPoint)];
+      [lines addObject:verticalLine];
+      [lines addObject:horizontalLine];
+    }
+    
+    // Apply params
+    for (SKShapeNode *lineNode in lines) {
+      lineNode.strokeColor = [UIColor blackColor];
+      lineNode.lineWidth = 1.0f;
+      
+      if (lineNode != _perimeterNode)
+        [_perimeterNode addChild:lineNode];
+      
+      // Dash
+      CGFloat pattern[2];
+      pattern[0] = 10.0;
+      pattern[1] = 10.0;
+      CGPathRef dashed = CGPathCreateCopyByDashingPath(lineNode.path, NULL, 0, pattern, 2);
+      lineNode.path = dashed;
+    }
   }
   return self;
 }
