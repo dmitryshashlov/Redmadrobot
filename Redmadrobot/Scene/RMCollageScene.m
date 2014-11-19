3ce0412219ec6c826a71c16faf7779295717cd72
Dmitry Shashlov <shashlov@gmail.com>
2014-11-19 19:34:10 +0300

    grid selection

--- Redmadrobot/Scene/RMCollageScene.m
+++ Redmadrobot/Scene/RMCollageScene.m
@@ -20,6 +20,9 @@ CGSize kCollageSize = { 288.0f , 288.0f };
 
 @interface RMCollageScene()
 @property (nonatomic) SKShapeNode *perimeterNode;
+@property (nonatomic) NSArray *selectedIndexPaths;
+@property (nonatomic) CGPoint selectionStartPoint;
+@property (nonatomic) SKShapeNode *selectionNode;
 @end
 
 @implementation RMCollageScene
@@ -47,6 +50,30 @@ CGSize kCollageSize = { 288.0f , 288.0f };
     
     // Configure
     [self configureSceneForStep:_step];
+    
+    // Observe selected index paths
+    [[RACObserve(self, selectedIndexPaths) distinctUntilChanged]
+     subscribeNext:^(NSArray *selectedIndexPaths) {
+       [_selectionNode removeFromParent];
+       if (selectedIndexPaths.count)
+       {
+         CGRect rect = CGRectZero;
+         for (NSIndexPath *indexPath in selectedIndexPaths) {
+           CGRect desiredRect = [[self class] rectForIndexPath:indexPath
+                                               withCollageSize:_collage.size.intValue];
+           if (rect.origin.x == 0 && rect.origin.y == 0 && rect.size.width == 0 && rect.size.height == 0)
+             rect = desiredRect;
+           else
+             rect = CGRectUnion(rect, desiredRect);
+         }
+         rect = CGRectOffset(rect, _perimeterNode.position.x, _perimeterNode.position.y);
+         
+         _selectionNode = [SKShapeNode shapeNodeWithRect:rect];
+         _selectionNode.strokeColor = [UIColor redColor];
+         _selectionNode.lineWidth = 2.0f;
+         [self addChild:_selectionNode];
+       }
+     }];
   }
   return self;
 }
@@ -151,18 +178,71 @@ CGSize kCollageSize = { 288.0f , 288.0f };
   CGPoint positionInScene = [touch locationInNode:self];
   SKShapeNode *node = (SKShapeNode *)[self nodeAtPoint:positionInScene];
   
+  self.selectionStartPoint = positionInScene;
+  
   // Log
   if ([node isKindOfClass:[RMSectorNode class]])
   {
     RMSectorNode *sectorNode = (RMSectorNode *)node;
     NSIndexPath *indexPath = sectorNode.sector.indexPath;
-    NSLog(@"Node (%@) touched: %d, %d", NSStringFromClass([node class]), indexPath.section, indexPath.row);
+    self.selectedIndexPaths = @[sectorNode.sector.indexPath];
+    NSLog(@"Touch started: %d, %d", indexPath.section, indexPath.row);
   }
   else if ([node isKindOfClass:[RMGroupNode class]])
   {
     RMGroupNode *groupNode = (RMGroupNode *)node;
     //
   }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+  UITouch *touch = [touches anyObject];
+  CGPoint positionInScene = [touch locationInNode:self];
+  self.selectedIndexPaths = [self indexPathsForRect:CGRectMake(MIN(_selectionStartPoint.x, positionInScene.x),
+                                                               MIN(_selectionStartPoint.y, positionInScene.y),
+                                                               fabs(_selectionStartPoint.x - positionInScene.x),
+                                                               fabs(_selectionStartPoint.y - positionInScene.y))];
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+  self.selectionStartPoint = CGPointZero;
+  self.selectedIndexPaths = nil;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - Private
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+- (NSArray *)indexPathsForRect:(CGRect)frame
+{
+  SKNode *node1 = [self nodeAtPoint:CGPointMake(frame.origin.x, frame.origin.y)];
+  SKNode *node2 = [self nodeAtPoint:CGPointMake(frame.origin.x + frame.size.width, frame.origin.y)];
+  SKNode *node3 = [self nodeAtPoint:CGPointMake(frame.origin.x + frame.size.width, frame.origin.y + frame.size.height)];
+  SKNode *node4 = [self nodeAtPoint:CGPointMake(frame.origin.x, frame.origin.y + frame.size.height)];
+  
+  NSArray *nodes = @[node1, node2, node3, node4];
+  
+  NSNumber *minSection = [nodes valueForKeyPath:@"@min.sector.indexPath.section"];
+  NSNumber *maxSection = [nodes valueForKeyPath:@"@max.sector.indexPath.section"];
+  NSNumber *minRow = [nodes valueForKeyPath:@"@min.sector.indexPath.row"];
+  NSNumber *maxRow = [nodes valueForKeyPath:@"@max.sector.indexPath.row"];
+  
+  NSMutableArray *indexPaths = [[NSMutableArray alloc] init];
+  for (int i = minSection.intValue ; i <= maxSection.intValue ; i++) {
+    for (int n = minRow.intValue ; n <= maxRow.intValue ; n++) {
+      NSIndexPath *indexPath = [NSIndexPath indexPathForRow:n inSection:i];
+      [indexPaths addObject:indexPath];
+    }
+  }
+  
+  NSLog(@"[%d] - min section: %d, max section: %d, min row: %d, max row: %d", indexPaths.count, minSection.intValue, maxSection.intValue, minRow.intValue, maxRow.intValue);
+
+  return [NSArray arrayWithArray:indexPaths];
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
